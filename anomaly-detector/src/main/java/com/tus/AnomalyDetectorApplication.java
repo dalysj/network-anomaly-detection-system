84c2850a42da9ec5d32cec27856d7645733f34d4
Stephen Daly <dalysj@protonmail.com>
2025-02-10 23:23:18 +0000

    Added SpringBootApplication classes and application.yaml files.

---
+++ anomaly-detector/src/main/java/com/tus/AnomalyDetectorApplication.java
@@ -0,0 +1,26 @@
+package com.tus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The AnomalyDetectorApplication class is the main class for the Anomaly
+ * Detector application.
+ * It is a Spring Boot application, and it uses the @SpringBootApplication
+ * annotation to enable auto-configuration and component scanning.
+ */
+@SpringBootApplication
+public class AnomalyDetectorApplication {
+
+    /**
+     * The main method serves as the entry point for the Anomaly Detector
+     * application.
+     * It uses Spring Boot's SpringApplication.run() method to launch the
+     * application.
+     *
+     * @param args Command line arguments passed to the application.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(AnomalyDetectorApplication.class, args);
+    }
+}
